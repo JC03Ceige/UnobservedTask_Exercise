e56e4b0ecd1a217a59126ae6707897b8f970b46d
jcvdmerwe03@gmail.com <jcvdmerwe03@gmail.com>
2020-11-17 20:43:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnobservedTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskPlaceOrders = Task.Run(() => PlaceOrders());
+            Task.Run(() => ProcessOrders());
+            Task.Run(() => ProcessOrders());
+            Task.Run(() => ProcessOrders());
+           
+
+            taskPlaceOrders.Wait();
+            Console.WriteLine("Press ENTER to finish");
+            Console.ReadLine();
+        }
+
+        static ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+        static void PlaceOrders()
+            {
+                for(int i = 1; 1 <= 100; i++)
+                {
+                    Thread.Sleep(250);
+                    String order = String.Format("Order {0}", i);
+                    queue.Enqueue(order);
+                    Console.WriteLine("Added {0}", order);
+                }
+            }
+
+        static void ProcessOrders()
+        {
+            string order;
+            while (true) //continue indefinitely
+
+                if (queue.TryDequeue(out order))
+                {
+                    Console.WriteLine("Processed {0}", order);
+                }
+        }
+    }
+}
